db8d04c6a50c9a98e460bdc3abe0c1292866912b
Deak, David <ddeak2@cscirapp1.local>
2016-04-19 04:07:27 -0500

    Initial Commit

---
+++ TimeToPlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TimeToPlay
+//
+//  Created by Deak, David on 4/19/16.
+//  Copyright © 2016 Deak, David. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
