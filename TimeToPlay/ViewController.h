e434495a4858aacf23696331c8f908ca376f3154
ddeak2 <david.p.deak@gmail.com>
2016-12-07 20:10:27 -0600

    Updates to functionality of game
    Added art and improved functionality of game.

--- TimeToPlay/ViewController.h
+++ TimeToPlay/ViewController.h
@@ -7,9 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "QuartzCore/CADisplayLink.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    CADisplayLink * theTimer;
+}
 
+@property (weak, nonatomic) IBOutlet UIImageView *theMeteor;
+@property (weak, nonatomic) IBOutlet UIImageView *theGround;
+@property (weak, nonatomic) IBOutlet UIImageView *David;
+@property (weak, nonatomic) IBOutlet UIButton *playAgainButton;
+@property (weak, nonatomic) IBOutlet UILabel *endLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel * score;
+@property (nonatomic) CGPoint firstTouch;
+@property (nonatomic) CGPoint lastTouch;
+
+- (IBAction)ResetGame:(id)sender;
 
 @end
 
--- TimeToPlay/ViewController.m
+++ TimeToPlay/ViewController.m
@@ -14,10 +14,103 @@
 
 @implementation ViewController
 
+ int theScore;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initializeTimer];
 }
+
+- (IBAction)ResetGame:(id)sender {
+    theScore = 0;
+    [self updateScore];
+    _theMeteor.center = CGPointMake(64, 64);
+    [self.David setImage: [UIImage imageNamed:@"David_shrunk"]];
+    _endLabel.hidden = true;
+    _playAgainButton.hidden = true;
+    _playAgainButton.enabled = false;
+    [self gameLogic:theTimer];
+    
+    
+}
+
+- (void)updateScore {
+    _score.text = [[NSString alloc] initWithFormat:@"%d",theScore];
+}
+
+
+- (void) initializeTimer {
+    if(theTimer == nil)
+    {
+        theTimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(gameLogic:)];
+    }
+    theTimer.frameInterval = 1;
+    [theTimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+- (void) gameLogic:(CADisplayLink *) theTimer {
+    if((!CGRectIntersectsRect(_theMeteor.frame, _David.frame)) && (theScore < 15)){
+        if(_endLabel.hidden == true){
+        if(_theMeteor.center.y >600){
+            int num = arc4random() % 3;
+            if(num == 0){
+                _theMeteor.center = CGPointMake(64, 64);
+            }
+            else if(num == 1){
+                _theMeteor.center = CGPointMake(192, 64);
+            }
+            else if(num == 2){
+                _theMeteor.center = CGPointMake(320, 64);
+            }
+            theScore = theScore + 1;
+            [self updateScore];
+            }
+            _theMeteor.center = CGPointMake(_theMeteor.center.x, _theMeteor.center.y+11);    }}
+    
+    else if(theScore == 15){
+        
+        _theMeteor.center = CGPointMake(_theMeteor.center.x, _theMeteor.center.y);
+        _endLabel.text = @"YOU WIN!";
+        _endLabel.hidden = false;
+        _playAgainButton.hidden = false;
+        _playAgainButton.enabled = true;
+        
+        
+    }
+    else{
+        _theMeteor.center = CGPointMake(64, 64);
+        _endLabel.text = @"YOU LOSE!";
+        _endLabel.hidden = false;
+        _playAgainButton.hidden = false;
+        _playAgainButton.enabled = true;
+       [self.David setImage: [UIImage imageNamed:@"David_shrunk_killed"]];
+    }
+    
+}
+
+- (void) touchesBegan:(NSSet *) touches withEvent:(UIEvent *) event {
+    UITouch *touch = [touches anyObject];
+    _firstTouch = [touch locationInView:self.view];
+    _lastTouch = [touch locationInView:self.view];
+    
+    _David.center = CGPointMake(_firstTouch.x, _David.center.y);
+}
+
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    _firstTouch = [touch locationInView:self.view];
+    _lastTouch = [touch locationInView:self.view];
+    
+    _David.center = CGPointMake(_firstTouch.x, _David.center.y);}
+
+-(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    _firstTouch = [touch locationInView:self.view];
+    _lastTouch = [touch locationInView:self.view];
+   
+    _David.center = CGPointMake(_firstTouch.x, _David.center.y);}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
